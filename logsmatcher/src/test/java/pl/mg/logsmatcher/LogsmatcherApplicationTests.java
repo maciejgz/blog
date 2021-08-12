c3be5faf514580513d78528b0d1529eb56848b2a
mgzik <mgzik@eversis.com>
2021-08-12 09:19:20 +0200

    Initial commit

---
+++ logsmatcher/src/test/java/pl/mg/logsmatcher/LogsmatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mg.logsmatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsmatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
